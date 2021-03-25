af31d432fd7a72b777da3237866a4fd0e2fef1a5
exitonoff <devausmm@gmail.com>
2021-03-25 17:40:58 +0200

    Add charting lib

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import App from './App.vue'
 import router from './router'
 import VueEllipseProgress from 'vue-ellipse-progress';
 import store from "./store"
+import TrendChart from "vue-trend-chart";
 Vue.config.productionTip = false
 
 Vue.use(VueEllipseProgress, "vep");// you can define a name and use the plugin like <vep/>
+Vue.use(TrendChart);
+
 new Vue({
   store,
   router,
